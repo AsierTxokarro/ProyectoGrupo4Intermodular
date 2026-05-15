e85e86c225be2756175e07a02152db8f5f96f114
Asier Txokarro Sota <asiertxokarro@gmail.com>
2026-05-15 10:14:18 +0200

    Añadidos harcodeados y cambios para funcion de tutor

--- Aplicacion/Module1.vb
+++ Aplicacion/Module1.vb
@@ -1,3 +1,21 @@
-﻿Module Module1
+﻿Imports Gestion
+
+Module Module1
     Public gestion As Gestion
+
+
+
+
+    ' Correo → contraseña
+    Public TutoresPass As New Dictionary(Of String, String) From {
+            {"tutor1@empresa.com", "1234"},
+            {"tutor2@empresa.com", "1234"}
+        }
+
+    ' Correo → lista de DNIs de sus alumnos
+    Public TutoresDNIs As New Dictionary(Of String, List(Of String)) From {
+            {"tutor1@empresa.com", New List(Of String) From {"12345678A", "87654321B"}},
+            {"tutor2@empresa.com", New List(Of String) From {"11111111C"}}
+        }
+
 End Module
