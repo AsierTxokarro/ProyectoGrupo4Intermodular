66afa641d2207746cf65f51e65202004bec70441
Adrian_Mivovan <adrianmivovan7@gmail.com>
2026-06-02 09:59:28 +0200

    Update FrmLogin.vb

--- Aplicacion/FrmLogin.vb
+++ Aplicacion/FrmLogin.vb
@@ -1,7 +1,7 @@
 ﻿Public Class FrmLogin
     Private Sub btnIniciarSesion_Click(sender As Object, e As EventArgs) Handles btnIniciarSesion.Click
         If gestionfrm.ValidarFormatoDNI(txtDNI.Text) Then
-            'TODO mostrar formulario correspondiente -> .Show()
+            FrmVerTareasAlumno.Show()
         Else
             MessageBox.Show("No se ha encontrado un usuario con ese DNI")
         End If
